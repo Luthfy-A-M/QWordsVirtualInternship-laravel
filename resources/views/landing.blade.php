500605014f2c396313c7720fc2413356e75a5a06
Luthfy-A-M <57748834+Luthfy-A-M@users.noreply.github.com>
2022-12-18 10:37:04 +0700

    create Search bar for getter

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -105,15 +105,22 @@
             <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+            <!-- Search form -->
+            <form>
+              <div class="form-group">
+                <label for="inputsm">Search Domain</label>
+                <input class="form-control input-sm" id="inputsm" type="text" value="this.domain">
+              </div>
+            </form>
+              
+            <!--end Search form-->
+            <p> 
+              Domain Yang Tersedia untuk this.domain
             </p>
             <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</li>
+              <li><i class="ri-check-double-line"></i> this.domain.co.id</li>
+              <li><i class="ri-check-double-line"></i> this.domain.org</li>
+              <li><i class="ri-check-double-line"></i> this.domain.com</li>
             </ul>
             <p class="fst-italic">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
